05b18f34d6c2244e41a53b8fb08b053e1c926a99
Vinodh Ganesan <vinodh@litmus7.com>
2023-02-06 15:54:15 +0530

    navbar

---
+++ navbar-mfe/src/root.component.tsx
@@ -0,0 +1,19 @@
+import './navamfe.css';
+import React from "react";
+
+export default function Root(props) {
+  return <nav>
+      <ul>
+        <li>
+          <a href="/">home</a>
+        </li>
+        <li>
+          <a href="/customer">Customer</a>
+        </li>
+        <li>
+          <a href="/order">Order</a>
+        </li>
+      </ul>
+  </nav>;
+    
+}
